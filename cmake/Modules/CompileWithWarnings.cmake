b38138c4b09f307ca8ec3c9f3c2f6c211b0f52be
Eyal Rozenberg <eyalroz1@gmx.com>
2022-04-30 23:17:49 +0300

    Regard #19: Fix up `CompileWithWarnings.cmake` module and actually use it.

--- cmake/Modules/CompileWithWarnings.cmake
+++ cmake/Modules/CompileWithWarnings.cmake
@@ -1,28 +1,9 @@
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    set(WARNING_FLAGS "-Wall -Wextra -Wpedantic -Wno-missing-field-initializers")
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    set(WARNING_FLAGS "-Wall -Wextra -Wpedantic -Wno-missing-field-initializers")
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
-    set(WARNING_FLAGS "-w3 -wd1418,2259")
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-    set(WARNING_FLAGS "/W4")
-else ()
-    message(WARNING "Unknown compiler - cannot set warning flags")
-endif()
-
-if(WARNING_FLAGS)
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${WARNING_FLAGS}")
-endif()
-
-macro(target_warning_options tgt)
-target_compile_options(tgt PRIVATE
-     $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
-          -Wall -Wextra -pedantic -Wshadow -Wdouble-promotion -Wformat=2 -Wsign-conversion -Wformat-overflow -Wformat-truncation -Wundef -fno-common -Wconversion -Wextra>
-           # Can add more errors here, I guess
-     $<$<CXX_COMPILER_ID:MSVC>:
-          /W4>
-     $<$<CXX_COMPILER_ID:Intel>:
-          -w3 -wd1418,2259>
+macro(target_warning_options the_tgt)
+	target_compile_options(${the_tgt} PRIVATE
+		$<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
+			-Wall -Wextra -pedantic -Wshadow -Wdouble-promotion -Wformat=2 -Wformat-overflow -Wformat-truncation -Wconversion -Wextra -Wno-missing-field-initializers>
+		$<$<CXX_COMPILER_ID:MSVC>: /W4>
+		$<$<CXX_COMPILER_ID:Intel>: -w3 -wd1418,2259>
 )
 endmacro()
 

e7951327eb2aa5f2bb4091df9e417dc3f5f65a64
Eyal Rozenberg <eyalroz1@gmx.com>
2022-06-05 11:43:43 +0300

    In the CMake code, distinguish between GCC-specific warnings and those common to GCC and clang.

--- cmake/Modules/CompileWithWarnings.cmake
+++ cmake/Modules/CompileWithWarnings.cmake
@@ -1,9 +1,12 @@
 macro(target_warning_options the_tgt)
 	target_compile_options(${the_tgt} PRIVATE
 		$<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
-			-Wall -Wextra -pedantic -Wshadow -Wdouble-promotion -Wformat=2 -Wformat-overflow -Wformat-truncation -Wconversion -Wextra -Wno-missing-field-initializers>
+			-Wall -Wextra -pedantic -Wshadow -Wdouble-promotion -Wformat=2 -Wconversion -Wno-missing-field-initializers>
+		$<$<CXX_COMPILER_ID:GNU>:
+			-Wformat-overflow -Wformat-truncation -Wconversion -Wextra -Wno-missing-field-initializers>
 		$<$<CXX_COMPILER_ID:MSVC>: /W4>
 		$<$<CXX_COMPILER_ID:Intel>: -w3 -wd1418,2259>
 )
+# For clang, we would have liked to use -Wno-c++17-extensions, but that probably requires a newer version
 endmacro()
 

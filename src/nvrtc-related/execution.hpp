1abe1e4ee94c276bd7e3df47379cea295e3e3e87
Eyal Rozenberg <eyalroz1@gmx.com>
2022-06-07 23:03:39 +0300

    Fixes #6: Now using the cuda-api-wrappers library extensively, including for everything compilation-related.

--- src/nvrtc-related/execution.hpp
+++ src/nvrtc-related/execution.hpp
@@ -2,57 +2,51 @@
 #define KERNEL_RUNNER_CUDA_EXECUTION_HPP_
 
 #include <common_types.hpp>
-#include <nvrtc-related/ugly_error_handling.hpp>
 
-void launch_time_and_sync_cuda_kernel(execution_context_t& context, run_index_t run_index)
+void launch_time_and_sync_cuda_kernel(execution_context_t& execution_context, run_index_t run_index)
 {
-    // TODO: This is ugly!
-    cuda_api_call(cuCtxPushCurrent, context.cuda.context);
-    void** alternative_format_arguments { nullptr };
-    const auto& lc = context.kernel_launch_configuration.cuda;
+    auto& cuda_context = *execution_context.cuda.context;
+    cuda::context::current::scoped_override_t cuda_context_for_this_scope(cuda_context);
+
+    const auto& lc = execution_context.kernel_launch_configuration.cuda;
     spdlog::info("Launching kernel {} (function name \"{}\")",
-        context.options.kernel.key,
-        context.options.kernel.function_name);
+                 execution_context.options.kernel.key,
+                 execution_context.options.kernel.function_name);
 
-    CUstream stream_handle;
-    cuda_api_call(cuStreamCreate, &stream_handle, CU_STREAM_NON_BLOCKING);
-    spdlog::debug("Created a non-blocking stream.");
-    // Sorry about this ugly code!
-    struct {
-        CUevent before, after;
-    } events;
-    if (context.options.time_with_events) {
-        spdlog::debug("Creating events & recording \"before\" event.");
-        cuda_api_call(cuEventCreate, &events.before, CU_EVENT_BLOCKING_SYNC);
-        cuda_api_call(cuEventCreate, &events.after, CU_EVENT_BLOCKING_SYNC);
-        cuda_api_call(cuEventRecord, events.before, stream_handle);
+    auto stream = cuda_context.create_stream(cuda::stream::async);
+    spdlog::debug("Created a non-blocking CUDA stream on device {}", cuda_context.device_id());
+    struct event_pair_t {
+        cuda::event_t before, after;
+    } ;
+    optional<event_pair_t> timing_events;
+    if (execution_context.options.time_with_events) {
+        spdlog::debug("Creating events & recording \"before\" event on the CUDA stream.");
+        timing_events = {
+            cuda_context.create_event(cuda::event::sync_by_blocking),
+            cuda_context.create_event(cuda::event::sync_by_blocking)
+        };
+        stream.enqueue.event(timing_events->before);
     }
+    auto mangled_kernel_signature = execution_context.cuda.mangled_kernel_signature->c_str();
+    auto kernel = execution_context.cuda.module->get_kernel(mangled_kernel_signature);
 
-    spdlog::debug("Passing {} arguments to the kernel.", context.finalized_arguments.pointers.size() - 1);
-    cuda_api_call(
-        cuLaunchKernel,
-        context.cuda.built_kernel,
-        lc.dimensions.grid.x,   lc.dimensions.grid.y, lc.dimensions.grid.z,
-        lc.dimensions.block.x, lc.dimensions.block.y, lc.dimensions.block.z,
-        lc.dynamic_shared_memory_size,
-        stream_handle,
-        const_cast<void**>(context.finalized_arguments.pointers.data()),
-        alternative_format_arguments
+    spdlog::debug("Passing {} arguments to kernel {}",
+        execution_context.finalized_arguments.pointers.size() - 1,
+        execution_context.options.kernel.function_name.c_str());
 
-    );
-    if (context.options.time_with_events) {
-        cuda_api_call(cuEventRecord, events.after, stream_handle);
+    cuda::launch_type_erased(kernel, stream, lc, execution_context.finalized_arguments.pointers);
+
+    if (execution_context.options.time_with_events) {
+        stream.enqueue.event(timing_events->after);
     }
-    cuda_api_call(cuStreamSynchronize, cuda::stream::default_stream_handle);
-    cuda_api_call_noargs(cuCtxSynchronize);
+    stream.synchronize();
 
-    if (context.options.time_with_events) {
-        float milliseconds_elapsed;
-        cuda_api_call(cuEventElapsedTime, &milliseconds_elapsed, events.before, events.after);
+    if (execution_context.options.time_with_events) {
+        auto duration = cuda::event::time_elapsed_between(timing_events->before, timing_events->after);
         spdlog::info("Event-measured time of run {} of kernel {}: {:.0f} nsec",
-            run_index+1, context.kernel_adapter_->kernel_function_name(), ((double) milliseconds_elapsed * 1000000.0));
+            run_index+1, execution_context.kernel_adapter_->kernel_function_name(), ((double) duration.count() * 1000000.0));
+            // TODO: Maybe there's a nicer way to print durations as nsecs?
     }
-    cuda_api_call(cuCtxPopCurrent, nullptr);
 }
 
 #endif // KERNEL_RUNNER_CUDA_EXECUTION_HPP_

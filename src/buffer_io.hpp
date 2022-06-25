653f753f7d873147fd01af1c4d10dfc84657f739
Eyal Rozenberg <eyalroz1@gmx.com>
2022-06-25 16:58:57 +0300

    Fixes #5: Added more file & directory status and permission checks
    CAVEAT: Permissions checks are enabled only on POSIX and Windows systems, and the Windows code is not tested.

--- src/buffer_io.hpp
+++ src/buffer_io.hpp
@@ -4,9 +4,26 @@
 #include <common_types.hpp>
 #include <spdlog/common.h>
 
-host_buffer_type read_input_file(filesystem::path src, size_t extra_buffer_size = 0);
+
+void verify_path(const filesystem::path& path, path_check_kind check_kind, bool allow_overwrite);
+host_buffer_type read_input_file(const filesystem::path& src, size_t extra_buffer_size = 0);
 host_buffer_type read_file_as_null_terminated_string(const filesystem::path& source);
-void write_data_to_file(std::string kind, std::string name, poor_mans_span data, filesystem::path destination, spdlog::level::level_enum level);
-void write_buffer_to_file(std::string buffer_name, const host_buffer_type& buffer, filesystem::path destination);
+void write_data_to_file(
+    std::string kind,
+    std::string name,
+    poor_mans_span data,
+    filesystem::path destination,
+    bool overwrite_allowed,
+    spdlog::level::level_enum level);
+void write_buffer_to_file(
+    std::string buffer_name,
+    const host_buffer_type& buffer,
+    filesystem::path destination,
+    bool overwrite_allowed);
+
+inline void verify_input_path(const filesystem::path& path)
+{
+    return verify_path(path, for_reading, false);
+}
 
 #endif /* BUFFER_IO_HPP_ */
--- src/util/filesystem.hpp
+++ src/util/filesystem.hpp
@@ -2,6 +2,13 @@
 #define UTIL_FILESYSTEM_HPP_
 
 #include <fstream>
+#if _POSIX_SOURCE
+#include <unistd.h>
+#else
+#ifdef WIN32
+#include <io.h>
+#endif
+#endif
 
 static_assert(__cplusplus >= 201402L, "C++2014 is required to compile this program");
 
@@ -58,5 +65,27 @@ inline void ensure_sized_file_existence(filesystem::path path, size_t size)
 //}
 //
 //} // namespace std
+
+enum path_check_kind { for_reading, for_writing, for_recursion };
+
+inline bool has_permission(const filesystem::path& path, path_check_kind permissions_kind)
+{
+#ifdef _POSIX_SOURCE
+    static constexpr int have_access{ 0 };
+    auto perm_flags =
+        (permissions_kind == for_reading) ? R_OK :
+        (permissions_kind == for_recursion) ? X_OK :
+        W_OK;
+    return (access(path.native().c_str(), perm_flags) == have_access);
+#else
+    #ifdef WIN32
+    if (permissions_kind == for_recrusion) { return; }
+    static constexpr int have_access{ 0 };
+    auto perm_flags = (permissions_kind == for_reading) ? 2 : 4;
+    return (_access_s( path.native.c_str(), perm_flags) == have_access);
+#endif
+#endif
+}
+
 
 #endif // UTIL_FILESYSTEM_HPP_

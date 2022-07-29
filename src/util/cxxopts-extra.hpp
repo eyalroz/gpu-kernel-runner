3ebc08c6a54203d27307c6a56467e15ca8d88fe2
Eyal Rozenberg <eyalroz1@gmx.com>
2022-07-29 22:29:36 +0300

    Broke the dependence of the kernel adapter class on the cxxopts library:
    * Instead of kernel adapters injecting options, the kernel runner can derive the necessary option information from the parameter details and preprocessor definition details the kernel adapters provide.
    * Added some convenience/utility functions for working with string options to `cxxopts-extra.hpp`, including a struct for specifying a string option.

--- src/util/cxxopts-extra.hpp
+++ src/util/cxxopts-extra.hpp
@@ -22,4 +22,30 @@ inline cxxopts::ParseResult non_consumptive_parse(cxxopts::Options& opts, int ar
     return opts.parse(argc, new_argv_data_copy);
 }
 
+struct string_option_spec {
+    const char* name;
+    const char* description;
+    const char* default_value; // nullptr for no default
+};
+
+inline void add_string_option(cxxopts::OptionAdder& adder, string_option_spec option_spec)
+{
+    adder(option_spec.name, option_spec.description,
+        option_spec.default_value ?
+        cxxopts::value<std::string>()->default_value(option_spec.default_value) :
+        cxxopts::value<std::string>());
+}
+
+template <typename StringOptionSpecContainer>
+inline void add_string_options(
+    cxxopts::Options& opts,
+    std::string option_group_name,
+    StringOptionSpecContainer&& string_options)
+{
+    auto adder = opts.add_options(option_group_name);
+    for(const string_option_spec& option_spec : string_options) {
+        add_string_option(adder, option_spec);
+    }
+}
+
 #endif // UTIL_CXXOPTS_EXTRA_HPP_

2ac231a18de4c9278db53b1907241f59c31ec046
Eyal Rozenberg <Eyal.Rozenberg@GEHealthCare.com>
2025-10-03 10:02:27 +0300

    Removed an inline function only used once, and an unnecessary include

--- src/cmdline_arguments.hpp
+++ src/cmdline_arguments.hpp
@@ -1,10 +1,3 @@
 #include "parsed_cmdline_options.hpp"
 
-#include <cxxopts/cxxopts.hpp>
-
 parsed_cmdline_options_t parse_command_line(int argc, char** argv);
-
-inline bool early_exit_needed(parsed_cmdline_options_t const& parsed)
-{
-    return parsed.early_exit_action.operator bool();
-}
